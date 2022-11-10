a787190286144b645ee13354c287989c84373f32
rajeshmel <117922279+rajeshmel@users.noreply.github.com>
2022-11-10 20:03:06 +0530

    Update streamlit_app.py
    added 4 lines for Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.write('Breakfast Menu')
+streamlit.write('Omega 3 & Blueberry Oatmeal')
+streamlit.write('Kale, Spinach and Rocket Smoothie')
+streamlit.write('Hard-Boiled Free-Range Egg')
