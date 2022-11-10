07ce619f69ea6f6749b80a383b9baf3116553a9c
rajeshmel <117922279+rajeshmel@users.noreply.github.com>
2022-11-10 20:06:18 +0530

    Update streamlit_app.py
    added header and used "text" instead of "write"

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.write('Breakfast Menu')
-streamlit.write('Omega 3 & Blueberry Oatmeal')
-streamlit.write('Kale, Spinach and Rocket Smoothie')
-streamlit.write('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
