c56e23831c178c0f03ef91600ece5c253d7170e5
rajeshmel <117922279+rajeshmel@users.noreply.github.com>
2022-11-15 12:22:01 +0530

    added try error block

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,16 +24,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+if not fruit_choice:
+   streamlit.write("Please select a fruit to get information!")
+else
+   #streamlit.write('The user entered ', fruit_choice)
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+   # streamlit.text(fruityvice_response.json())
+   # Takes the json version of response and normalize it
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   # present the output in tabular format
+   streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json())
+except URLError as e:
+   streamlit.error()
 
-# Takes the json version of response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# present the output in tabular format
-streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 
